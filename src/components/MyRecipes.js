08f75751706a35d567e08abc5857c00e89ceeaab
Mauricio Tomaz <mauriciotomazz@gmail.com>
2022-09-01 17:02:21 -0700

    Implement My Recipes component

--- src/components/MyRecipes.js
+++ src/components/MyRecipes.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { useAppContext } from '../../context/appContext';
+import { FaTrash } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import { truncateRecipeTitle } from '../shared/utils';
 
-const Recipes = () => {
-    return <p>Recipes</p>
+const MyRecipes = () => {
+    const { state, dispatch } = useAppContext();
+
+    const removeRecipe = (recipeId) => {
+        const payload = { recipeId };
+        dispatch({
+            type: 'removeRecipe',
+            payload,
+        });
+    }
+
+    return (
+        <div className="p-1">
+            <h4 className="mb-4">My Recipes </h4>
+            <ul className="list-unstyled">
+                {state.recipes.map((r) => (
+                    <li key={r.id} style={{ fontSize: '0.8rem' }} className="mb-2">
+                        <FaTrash className="me-2 text-danger" onClick={() => removeRecipe(r.id)} /> {truncateRecipeTitle(r.title)}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
-export default Recipes;
+export default MyRecipes;
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,15 +1,38 @@
 import { FaBookmark } from 'react-icons/fa';
 import { Card } from 'react-bootstrap';
+import { useAppContext } from '../context/appContext'; 
+import { truncateRecipeTitle } from '../shared/utils'; 
+import { addRecipe } from '../services/recipeService'
 
 const RecipeCard = ({ recipe }) => {
+    const { state, dispatch } = useAppContext();
+
+    const bookmarkRecipe = (recipe) => {
+        const payload = {
+            newRecipe: recipe,
+        };
+        dispatch({ type: 'addRecipe', payload });
+        addRecipe(recipe, state.user.uid);
+    }
+
+    const isDisabled = (recipeId) => {
+        return state.recipes.some(r => r.id === recipeId);
+    }
+
     return (
         <div className="w-25 p-1" key={recipe.id}>
             <Card>
                 <Card.Img variant="top" src={recipe.image} />
                 <Card.Body>
-                    <Card.Title style={{ height: '3rem' }}>{recipe.title}</Card.Title>
+                    <Card.Title style={{ height: '3rem' }}>{truncateRecipeTitle(recipe.title)}</Card.Title>
                     <Card.Text className="d-flex justify-content-end">
-                        <FaBookmark className="cursor-pointer" />
+                        {!isDisabled(recipe.id) && (
+                            <FaBookmark
+                                style={{ cursor: 'pointer' }}
+                                className="text-success"
+                                onClick={() => bookmarkRecipe(recipe)}
+                            />
+                        )}
                     </Card.Text>
                 </Card.Body>
             </Card>
