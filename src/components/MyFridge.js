f85a65e2a394f47643b071395dd58ff39cedda96
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-09-06 13:24:42 -0700

    My fridge part

--- src/components/MyFridge.js
+++ src/components/MyFridge.js
@@ -1,8 +1,55 @@
-import React from 'react';
-import { useAppContext } from '../../context/appContext';
+import React, { useState } from 'react';
+import { getIngredients } from '../services/ingredientService';
+import RecipeCard from './RecipeCard';
 
 const Recipes = () => {
-    return <p>Recipes</p>
+    const [data, setData] = useState([]);
+    const [formData, setFormData] = useState({
+        query: '',
+    });
+    console.log(data)
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        getIngredients(formData.query).then(result => {setData(result.data.results) 
+            console.log(data.results)}
+    
+        )
+    
+    }
+    
+   
+
+    return ( 
+    <div className='w100 p-1'>
+        <form onSubmit={handleSubmit}>
+            <h4>My Fridge</h4>
+            <input
+                type='text'
+                name='query'
+                id='query'
+                className='w100 p-1'
+                placeholder='Search Ingredients'
+                value={formData.query}
+                onChange={({ target }) => setFormData ({...formData, query: target.value})}
+                
+            />
+            <input 
+                type='submit'
+                value='Add'
+                className='w100 p-1'
+            />
+
+        </form>
+        <div className='d-flex flex-wrap' >
+            {data.map(row => (
+                <RecipeCard recipe={row} />
+            ))}
+        </div>
+        
+
+
+    </div>
+    )
 }
 
 export default Recipes;
